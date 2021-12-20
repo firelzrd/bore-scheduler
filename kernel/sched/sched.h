a9366cd10698fe2cd1febfc95516a8b43684f7e6
Masahito S <firelzrd@gmail.com>
2021-12-21 02:48:46 +0900

    bore16-cacule

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -534,14 +534,9 @@ struct cfs_rq {
 	unsigned int		idle_h_nr_running; /* SCHED_IDLE */
 
 	u64			exec_clock;
-	u64			min_vruntime;
+
 #ifdef CONFIG_SCHED_CORE
 	unsigned int		forceidle_seq;
-	u64			min_vruntime_fi;
-#endif
-
-#ifndef CONFIG_64BIT
-	u64			min_vruntime_copy;
 #endif
 
 	struct rb_root_cached	tasks_timeline;
@@ -551,9 +546,8 @@ struct cfs_rq {
 	 * It is set to NULL otherwise (i.e when none are currently running).
 	 */
 	struct sched_entity	*curr;
-	struct sched_entity	*next;
-	struct sched_entity	*last;
-	struct sched_entity	*skip;
+	struct bs_node	*head;
+	struct bs_node	*tail;
 
 #ifdef	CONFIG_SCHED_DEBUG
 	unsigned int		nr_spread_over;
@@ -2307,6 +2301,7 @@ extern void schedule_idle(void);
 
 extern void sysrq_sched_debug_show(void);
 extern void sched_init_granularity(void);
+extern void sched_init_bs_sched(void);
 extern void update_max_interval(void);
 
 extern void init_sched_dl_class(void);
