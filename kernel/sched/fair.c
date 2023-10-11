1504de78fe0f5b6f41238e32ba889c13528fcab4
Masahito S <firelzrd@gmail.com>
2023-10-11 17:39:21 +0900

    Renamed: penalty_scale -> penalty_scale_mayhalve

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -163,11 +163,15 @@ static void update_burst_penalty(struct sched_entity *se) {
 	se->burst_penalty = max(se->prev_burst_penalty, se->curr_burst_penalty);
 }
 
-static inline u64 penalty_scale(u64 delta, struct sched_entity *se, bool half) {
+static inline u64
+penalty_scale_mayhalve(u64 delta, struct sched_entity *se, bool half) {
 	u32 score = ((x16*)&se->burst_penalty)->u8[1];
 	if (half) score >>= 1;
 	return mul_u64_u32_shr(delta, prio_to_wmult[score], 22);
 }
+
+#define penalty_scale(delta, se) \
+        (penalty_scale_mayhalve(delta, se, false))
 
 static inline u32 binary_smooth(u32 new, u32 old) {
   int increment = new - old;
@@ -185,7 +189,7 @@ static void restart_burst(struct sched_entity *se) {
 
 static inline void vruntime_backstep(s64 *vdiff, struct sched_entity *se) {
 	u64 delta_exec = se->sum_exec_runtime - se->prev_sum_exec_runtime;
-	*vdiff += delta_exec - penalty_scale(delta_exec, se, false);
+	*vdiff += delta_exec - penalty_scale(delta_exec, se);
 }
 #endif // CONFIG_SCHED_BORE
 
@@ -701,7 +705,7 @@ static inline u64 calc_delta_fair(u64 delta, struct sched_entity *se)
 		delta = __calc_delta(delta, NICE_0_LOAD, &se->load);
 
 #ifdef CONFIG_SCHED_BORE
-	if (likely(sched_bore)) delta = penalty_scale(delta, se, half);
+	if (likely(sched_bore)) delta = penalty_scale_mayhalve(delta, se, half);
 #endif // CONFIG_SCHED_BORE
 	return delta;
 }
