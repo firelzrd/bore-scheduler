0e9f874bc2960ab4d7a8a878e9f43b6641943ab8
Masahito S <firelzrd@gmail.com>
2024-07-04 19:12:50 +0900

    sched_burst_wakeup_boost: 1 = subtract burst_time / 4, 0 = do nothing

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -110,7 +110,6 @@ u8   __read_mostly sched_burst_wakeup_boost     = 0;
 u8   __read_mostly sched_burst_penalty_offset   = 22;
 uint __read_mostly sched_burst_penalty_scale    = 1280;
 uint __read_mostly sched_burst_cache_lifetime   = 60000000;
-static int __maybe_unused seven          = 7;
 static int __maybe_unused sixty_four     = 64;
 static int __maybe_unused maxval_12_bits = 4095;
 
@@ -299,7 +298,7 @@ static struct ctl_table sched_fair_sysctls[] = {
 		.mode		= 0644,
 		.proc_handler = proc_dou8vec_minmax,
 		.extra1		= SYSCTL_ZERO,
-		.extra2		= &seven,
+		.extra2		= SYSCTL_ONE,
 	},
 	{
 		.procname	= "sched_burst_penalty_offset",
@@ -5378,11 +5377,11 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq) {}
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 {
-	u64 vslice, vruntime = avg_vruntime(cfs_rq);
+	u64 deadline, vslice, vruntime = avg_vruntime(cfs_rq);
 	s64 lag = 0;
 
 	se->slice = sysctl_sched_base_slice;
-	vslice = calc_delta_fair(se->slice, se);
+	deadline = vslice = calc_delta_fair(se->slice, se);
 
 	/*
 	 * Due to how V is constructed as the weighted average of entities,
@@ -5470,27 +5469,17 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	 * on average, halfway through their slice, as such start tasks
 	 * off with half a slice to ease into the competition.
 	 */
-	bool half_flag;
-	if (flags & ENQUEUE_INITIAL) {
-		if (!(half_flag = sched_feat(PLACE_DEADLINE_INITIAL)))
-			goto no_boost;
+	if (flags & ENQUEUE_WAKEUP  ||
+	    flags & ENQUEUE_INITIAL && sched_feat(PLACE_DEADLINE_INITIAL)) {
+		deadline >>= 1;
+		if (sched_burst_wakeup_boost && (vslice > se->burst_time))
+			deadline -= (vslice - se->burst_time) >> 2;
 	}
-	else if (flags & ENQUEUE_WAKEUP)
-		half_flag = sched_burst_wakeup_boost & 0x4;
-	else
-		goto no_boost;
-
-	u64 orig_vslice = vslice;
-	vslice >>= half_flag;
-	u32 lag_flag = sched_burst_wakeup_boost & 0x3;
-	if (lag_flag && (orig_vslice > se->burst_time))
-		vslice -= (orig_vslice - se->burst_time) >> (lag_flag - 1);
-no_boost:
 
 	/*
 	 * EEVDF: vd_i = ve_i + r_i/w_i
 	 */
-	se->deadline = se->vruntime + vslice;
+	se->deadline = se->vruntime + deadline;
 }
 
 static void check_enqueue_throttle(struct cfs_rq *cfs_rq);
