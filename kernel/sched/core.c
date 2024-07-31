b5327535e2d76c5a20c1ce22f7bd2c4aca2e0ab1
Masahito S <firelzrd@gmail.com>
2024-08-01 02:38:58 +0900

    version number 5.1.6 -> 5.1.8

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -10103,7 +10103,7 @@ void __init sched_init(void)
 
 #ifdef CONFIG_SCHED_BORE
 	sched_init_bore();
-	printk(KERN_INFO "BORE (Burst-Oriented Response Enhancer) CPU Scheduler modification 5.1.6 by Masahito Suzuki");
+	printk(KERN_INFO "BORE (Burst-Oriented Response Enhancer) CPU Scheduler modification 5.1.8 by Masahito Suzuki");
 #endif // CONFIG_SCHED_BORE
 
 	wait_bit_init();
