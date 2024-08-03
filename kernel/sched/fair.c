73a5a6bab4c4a791d317a721c2154ebfc1a05c7d
Masahito S <firelzrd@gmail.com>
2024-08-04 02:36:49 +0900

    update_curr(): Don't use bottom limit of 1ULL for calc_delta_fair()

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1479,10 +1479,8 @@ static void update_curr(struct cfs_rq *cfs_rq)
 #ifdef CONFIG_SCHED_BORE
 	curr->burst_time += delta_exec;
 	update_burst_penalty(curr);
-	curr->vruntime += max(1ULL, calc_delta_fair(delta_exec, curr));
-#else // !CONFIG_SCHED_BORE
-	curr->vruntime += calc_delta_fair(delta_exec, curr);
 #endif // CONFIG_SCHED_BORE
+	curr->vruntime += calc_delta_fair(delta_exec, curr);
 	update_deadline(cfs_rq, curr);
 	update_min_vruntime(cfs_rq);
 
