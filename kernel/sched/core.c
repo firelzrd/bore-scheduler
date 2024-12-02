6ed99aebc91703a40f732899b9c391e3e89706de
Masahito S <firelzrd@gmail.com>
2024-12-03 02:54:53 +0900

    Added sched_bore_init() and moved BORE-related scheduler initialization code to bore.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9949,8 +9949,7 @@ void __init sched_init(void)
 #endif
 
 #ifdef CONFIG_SCHED_BORE
-	printk(KERN_INFO "BORE (Burst-Oriented Response Enhancer) CPU Scheduler modification %s by Masahito Suzuki", SCHED_BORE_VERSION);
-	init_task_bore(&init_task);
+	sched_bore_init();
 #endif // CONFIG_SCHED_BORE
 
 	wait_bit_init();
