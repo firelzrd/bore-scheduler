c22bf5e9fbc11319e6fe973d28f544a64a1ac8da
Masahito S <firelzrd@gmail.com>
2024-12-26 08:02:51 +0900

    Separated burst_cache_lock to two individual locks for child_burst/group_burst

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -553,6 +553,7 @@ struct sched_burst_cache {
 	u8				score;
 	u32				count;
 	u64				timestamp;
+    spinlock_t		lock;
 };
 #endif // CONFIG_SCHED_BORE
 
@@ -577,7 +578,6 @@ struct sched_entity {
 	u8				curr_burst_penalty;
 	u8				burst_penalty;
 	u8				burst_score;
-    spinlock_t		burst_cache_lock;
 	struct sched_burst_cache child_burst;
 	struct sched_burst_cache group_burst;
 #endif // CONFIG_SCHED_BORE
