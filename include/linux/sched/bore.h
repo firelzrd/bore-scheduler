63fa93e09ed8b7a222debcb8b3e6193d1e8d9aad
Masahito S <firelzrd@gmail.com>
2024-12-27 16:10:07 +0900

    version number 5.9.1 -> 5.9.3

--- include/linux/sched/bore.h
+++ include/linux/sched/bore.h
@@ -4,7 +4,7 @@
 
 #ifndef _LINUX_SCHED_BORE_H
 #define _LINUX_SCHED_BORE_H
-#define SCHED_BORE_VERSION "5.9.1"
+#define SCHED_BORE_VERSION "5.9.3"
 
 #ifdef CONFIG_SCHED_BORE
 extern u8   __read_mostly sched_bore;
